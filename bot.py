799ca86260a070241626facb1ef29037698f110d
Ihor-Chupryna <igchup1985@gmail.com>
2024-04-16 00:21:22 +0300

    del function is_digit

--- bot.py
+++ bot.py
@@ -3,53 +3,20 @@ import telebot
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 from database import limit_users, check_user_in_db, create_table, insert_data, count_all_symbol
 from config import config, bot_token
-# from validators import is_tts_symbol_limit
-
+from validators import is_tts_symbol_limit
 
 bot = telebot.TeleBot(token=bot_token)
-
-users = {}
-
-
-def create_buttons(list_buttons: list):
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-    for button in list_buttons:
-        keyboard.add(KeyboardButton(button))
-
-    return keyboard
-
-
-def is_tts_symbol_limit(message):
-    user_id = message.from_user.id
-    text_symbols = len(message.text)
-
-    # Функция из БД для подсчёта всех потраченных пользователем символов
-    all_symbols = count_all_symbol(user_id) + text_symbols
-
-    # Сравниваем all_symbols с количеством доступных пользователю символов
-    if all_symbols >= int(config['LIMITS']['MAX_TTS_SYMBOLS']):
-        msg = (f"Превышен общий лимит SpeechKit TTS {config['LIMITS']['MAX_TTS_SYMBOLS']}. Использовано: "
-               f"{all_symbols} символов. Доступно: {int(config['LIMITS']['MAX_TTS_SYMBOLS']) - all_symbols}")
-        bot.send_message(user_id, msg)
-        return None
-
-    # Сравниваем количество символов в тексте с максимальным количеством символов в тексте
-    if text_symbols >= int(config['LIMITS']['MAX_USER_TTS_SYMBOLS']):
-        msg = f"Превышен лимит SpeechKit TTS на запрос {config['LIMITS']['MAX_USER_TTS_SYMBOLS']}, в сообщении {text_symbols} символов"
-        bot.send_message(user_id, msg)
-        return None
-    return len(message.text)
 
 
 @bot.message_handler(commands=['start'])
 def say_start(message):
     bot.send_message(message.chat.id, f'hello!!!!!!!!!!!!')
-
     create_table()
+
     user_id = message.from_user.id
-    user_name = message.from_user.first_name
     limit = limit_users()
     checked_user = check_user_in_db(user_id)
+
     if limit and not checked_user:
         bot.send_message(message.chat.id, f'limit!!!!!!!!!!!!')
     else:
@@ -67,9 +34,6 @@ def tts_handler(message):
     user_id = message.from_user.id
     checked_user = check_user_in_db(user_id)
     if checked_user:
-        users[user_id] = {
-            'tts_symbols': ''
-        }
         bot.send_message(message.chat.id, f"Введите текст для генерации")
         bot.register_next_step_handler(message, text_to_speech)
     else:
@@ -78,12 +42,21 @@ def tts_handler(message):
 
 def text_to_speech(message):
     user_id = message.from_user.id
-    print(message.text)
 
-    symbols = is_tts_symbol_limit(message)
-    insert_data(user_id, message.text, symbols)
-    print('===================================')
-    print(symbols)
+    if message.text.isdigit():
+        bot.send_message(message.chat.id, 'Вветите текст, а не число!!')
+
+    else:
+        symbols, msg = is_tts_symbol_limit(message)
+
+        if not symbols:
+            bot.send_message(message.chat.id, msg)
+
+        else:
+            print(message.text)
+
+            insert_data(user_id, message.text, symbols)
+
     bot.register_next_step_handler(message, text_to_speech)
 
 
